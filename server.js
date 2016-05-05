ec706d8b1f57ef6d553bf77e2f1fb23b5f994f4a
Biyas Basak <basakbiyas001@gmail.com>
2016-05-06 02:46:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var app = express();
+
+
+var port = process.env.PORT || 3000;
+var secport = port + 443;
+
+// Create HTTP server.
+var server = http.createServer(app);
+
+// Create HTTPS server.
+var options = {
+	key: fs.readFileSync(__dirname + '/certificate/private.key'),
+	cert: fs.readFileSync(__dirname + '/certificate/certificate.pem')
+};
+
+var secureServer = https.createServer(options, app);
+
+// redirecting all requests to https 
+app.all('*', (req, res, next)=>{
+	if (req.secure) {
+		return next();
+	};
+	res.redirect('https://' + req.hostname + ':' + secport + req.url);
+});
+
+app.get('/', (req, res) => {
+	res.writeHead(200);
+	res.end("hello world\n");
+});
+
+server.listen(port, ()=>{
+	console.log('server listening on port', port);
+})
+
+secureServer.listen(secport, () => {
+	console.log('server listening on port', secport);
+
+});
